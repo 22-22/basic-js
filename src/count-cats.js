2b3789cf82026ba2ad5b052259b4dde2919fdc53
Yuliya Kogol <kogol.julia@gmail.com>
2020-02-23 22:30:33 +0300

    feat: create function for counting cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,20 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix)
+{    
+  let number = 0;
+
+  //iterate over outer array
+  for (let i = 0; i < matrix.length; i++) {
+    //create variable for inner array length
+    let innerLength = matrix[i].length;
+
+    //iterate over inner array
+    for (let j = 0; j < innerLength; j++) {
+      if (matrix[i][j] == '^^') {
+        number++;
+      }
+    }
+  }
+
+  return number; 
+}
+
