653510983e7fe9cfe2b541f994d7e485bedf3ab6
Yuliya Kogol <kogol.julia@gmail.com>
2020-02-26 14:46:17 +0300

    feat: make function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,23 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+    let name = [];
+
+    //check if the argument is an array
+    if (!Array.isArray(members)) return false;
+
+    //for each element of the array
+    members.forEach(item => {
+        //ignore non-string elements in the array
+        if (typeof item !== 'string') return false;
+        //remove whitespaces from the beginning and the end of the string item
+        let itemTrimmed = item.trim();
+        //make the first character of the string item uppercase
+        let itemUpperCase = itemTrimmed[0].toUpperCase();
+        //push the first character of the string item to the new array
+        name.push(itemUpperCase);
+    });
+
+    name.sort();
+    let result = name.join('');
+    return result;
 };
