5ff6dd6117a3801009be757825e536a179be09f5
Yuliya Kogol <kogol.julia@gmail.com>
2020-02-29 18:13:02 +0300

    feat: create the object chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,41 @@
 const chainMaker = {
+
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    //if (this.chain === undefined)
+    if (!this.chain) this.chain = [];
+    this.chain.push(`( ${value} )`);
+    //returns the object so that we can continue invoking methods
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position <= 0 || position > this.chain.length || typeof position !== 'number') {
+      delete this.chain;
+      throw new Error();
+    };
+    //position is a number starting from 1 while elements are counted from 0
+    this.chain.splice(position - 1, 1);
+    //returns the object so that we can continue invoking methods
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    //if (this.chain === undefined)
+    if (!this.chain) this.chain = [];
+    this.chain.reverse();
+    //returns the object so that we can continue invoking methods
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    let str = this.chain.join('~~');
+    //delete the existing chain
+    delete this.chain;
+    return str;
+  },
+
 };
 
 module.exports = chainMaker;
