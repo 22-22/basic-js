64dc09c96f256c6e3fd508e1cc02ed85200d0d70
Yuliya Kogol <kogol.julia@gmail.com>
2020-03-03 13:19:07 +0300

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,32 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    let arr = [];
+
+    //if repeatTimes and additionRepeatTimes are not defined, the corresponding string is not repeated
+    if (!options.repeatTimes) arr.push(str);
+    if (!options.additionRepeatTimes) arr.push(options.addition);
+
+    for (let i = 1; i <= options.repeatTimes; i++) {
+        //str should be string
+        arr.push(String(str));
+
+        for (let j = 1; j <= options.additionRepeatTimes; j++) {
+
+            //addition should be string
+            arr.push(String(options.addition));
+
+            //push the additionSeparator after every addition but the last one
+            if (j < options.additionRepeatTimes) 
+                arr.push(options.additionSeparator || '|');
+        }
+        
+        //push the separator after every str but the last one
+        if (i < options.repeatTimes) 
+            arr.push(options.separator || '+');
+    }
+
+    let result = arr.join('');
+
+    return result;
+
 };
-  
